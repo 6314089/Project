5e1c5f8f17294b107f9696324a23a61bb6b1e63d
6314089 <6314089@ed.tus.ac.jp>
2016-10-15 23:24:38 +0900

    入力された文字列をswitchで判別して好きな種類の音を出力できるように

--- 6314089/01/create_sound.m
+++ 6314089/01/create_sound.m
@@ -1,2 +1,16 @@
 function create_sound(filename, T, type, f)
+    Fs = 44100;
+    t = 0:(1/Fs):T;
     
+    switch type
+        case 'sin'
+            audiowrite(filename, sin(2*pi*f*t), Fs)
+        case 'saw'
+            audiowrite(filename, sawtooth(2*pi*f*t), Fs)
+        case 'tri'
+            audiowrite(filename, sawtooth(2*pi*f*t, 0.5), Fs)
+        case 'squ'
+            audiowrite(filename, square(2*pi*f*t), Fs)
+        otherwise
+            disp('Error: type is one of: sin, saw, tri, squ')
+    end
