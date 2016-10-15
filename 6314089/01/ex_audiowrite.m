de1e3732ae740c568d3fc71a784166ccfef3b6a9
6314089 <6314089@ed.tus.ac.jp>
2016-10-15 22:47:25 +0900

    ノコギリ波を追加

--- 6314089/01/ex_audiowrite.m
+++ 6314089/01/ex_audiowrite.m
@@ -3,6 +3,13 @@ Fs = 44100;
 T = 1;
 t = 0:(1/Fs):T;
 
-y = sin(2*pi*f*t);
 
-audiowrite('sin.wav', y, Fs)
+y1 = sin(2*pi*f*t);
+
+audiowrite('sin.wav', y1, Fs)
+
+
+y2 = sawtooth(2*pi*f*t);
+
+audiowrite('saw.wav', y2, Fs)
+
