c507a6e5f22f8420e0df675b75dbf599e81989a7
Michael Laurents <you know@gmail.com>
2016-10-15 18:49:29 +0900

    素材たちで軽い分離

---
+++ Jsimluken/separate_ways/separater.m
@@ -0,0 +1,10 @@
+function res = separater(org,src,dst)
+    [maxSrc,mIndexSrc] = max(src);
+    [maxDst,mIndexDst] = max(dst);
+    syms u v;
+    S = solve([u*src(mIndexSrc)+v*dst(mIndexSrc)==org(mIndexSrc),u*src(mIndexDst)+v*dst(mIndexDst)==org(mIndexDst)],[u,v]);
+    disp(maxDst);
+    u = double(S.u);
+    v = double(S.v);
+    res = (org - u*src)/v;
+    return 
