2a8f0d4c39c36a9f2a8913f981ffcb23b63113ae
6314089 <6314089@ed.tus.ac.jp>
2016-10-16 17:45:11 +0900

    複数同時に鳴らせるように

--- 6314089/01/create_sound.m
+++ 6314089/01/create_sound.m
@@ -1,16 +1,25 @@
-function create_sound(filename, T, type, f)
+function create_sound(filename, T, varargin)
     Fs = 44100;
     t = 0:(1/Fs):T;
     
-    switch type
-        case 'sin'
-            audiowrite(filename, sin(2*pi*f*t), Fs)
-        case 'saw'
-            audiowrite(filename, sawtooth(2*pi*f*t), Fs)
-        case 'tri'
-            audiowrite(filename, sawtooth(2*pi*f*t, 0.5), Fs)
-        case 'squ'
-            audiowrite(filename, square(2*pi*f*t), Fs)
-        otherwise
-            disp('Error: type is one of: sin, saw, tri, squ')
+    y = zeros(1, length(t));
+    
+    n = floor(length(varargin)/2);
+    for k = 1:n
+        type = varargin{1+2*(k-1)};
+        f = varargin{2+2*(k-1)};
+        switch type
+            case 'sin'
+                y = y + sin(2*pi*f*t);
+            case 'saw'
+                y = y + sawtooth(2*pi*f*t);
+            case 'tri'
+                y = y + sawtooth(2*pi*f*t, 0.5);
+            case 'squ'
+                y = y + square(2*pi*f*t);
+            otherwise
+                disp('Error: type is one of: sin, saw, tri, squ')
+        end
     end
+    
+    audiowrite(filename, y, Fs)
