2b2d2550701ac0b6557e462ce9e2564c3c1b180f
6314089 <6314089@ed.tus.ac.jp>
2016-10-18 07:50:20 +0900

    途中経過

--- 6314089/03/A_frequencies.m
+++ 6314089/03/A_frequencies.m
@@ -1,19 +1,41 @@
-function As = A_frequencies(filename)
+function ret = A_frequencies(filename)
+    %read file
     [input, Fs] = audioread(filename);
     
+    %fft
     m = length(input); %window length
     n = 2^nextpow2(m);
     Y = fft(input, n);
     
+    %get peek frequencies and length
     [pks, locs] = findpeaks(abs(Y), 'MinPeakProminence', 2500);
-    fs = (locs-1)*(Fs/n);
+    fs = (locs-1)*(Fs/n); %turn locs into 0 start and frequencies
     nfs = ceil(length(fs)/2);
     
+    %get key A frequencies and length
     Asf = get_A;
+    nAsf = length(Asf);
+       
+    %fill return value with 0
+    ret = zeros(1, length(Asf));
+
+    %
+    ratio = 0;    
+
     
-    As = zeros(1, length(Asf));
-    for Af = Asf
-        for k = 1:nfs
-            f= fs(k)
+    for i = 1:nAsf
+        Af = Asf(i);
+        for j = 1:nfs
+            f= fs(j);
+            
+            if Af >= f
+                ratio = Af / f;
+            else
+                ratio = f / Af;
+            end
+            
+            if ratio <= threshold
+               ret(i) = pks(j);
+            end
         end
     end
