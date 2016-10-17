734dcf765a78ddaa88f45990a1274ddc5e0c76c5
Michael Laurents <you know@gmail.com>
2016-10-17 22:05:56 +0900

    バグまみれだった.....

--- Jsimluken/evalate_spectrum/evalSpectrum.m
+++ Jsimluken/evalate_spectrum/evalSpectrum.m
@@ -1,9 +1,12 @@
 function evalValue = evalSpectrum(org,src)
     evalValue = 0;
-    searchWidth = len(org);
+    searchWidth = size(org);
     searchWidth = floor(searchWidth(1)/2) +...
-                  mod(searchWidth,2); %because of symmetric
+                  mod(searchWidth(1),2); %because of symmetric
     for i = 1:searchWidth
        evalValue = evalValue + (src(i)-org(i))^2;
+       
+      
     end
+    evalValue = evalValue / searchWidth;
     return
