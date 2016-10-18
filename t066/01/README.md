baf2dad8a1bd78887ee8ac32cc1366d490d03c61
t066 <6314066@ed.tus.ac.jp>
2016-10-18 13:22:10 +0900

    単一音源から基音周波数を判定するMPMアルゴリズムを書く

---
+++ t066/01/README.md
@@ -0,0 +1,5 @@
+﻿#00
+単一音源の基音周波数判定用MPMアルゴリズムを書く
+
+#nsdf.m
+Normalized Square Difference Functionを計算する。
---
+++ t066/01/nsdf.m
@@ -0,0 +1,9 @@
+%*********************************
+% returns NSDF of input signal
+% Pre:
+%       signal = windowed signal
+%*********************************
+function nsdfCor = nsdf(signal)
+    
+
+end
