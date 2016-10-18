048fd153e53ccf6d5b2608607da00c501c614fef
6314089 <6314089@ed.tus.ac.jp>
2016-10-18 21:16:55 +0900

    テストで音を実際に読み込んでみる．

---
+++ 6314089/03/test1.m
@@ -0,0 +1,13 @@
+%path = '../Audio/A4_sin.wav';
+%path = '../Audio/A4_saw.wav';
+%path = '../Audio/A4_tri.wav';
+%path = '../Audio/A4_squ.wav';
+path = '../Audio/A4_tri_F4_tri.wav';
+
+[audio, Fs] = audioread(path);
+a4saw = A_frequencies(path);
+
+
+sound(audio, Fs);
+pause(2.5);
+play_a(a4saw, Fs, 2);
