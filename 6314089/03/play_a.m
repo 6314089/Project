f2d047990827b021171e5ebb28c2b7800ca0bd60
6314089 <6314089@ed.tus.ac.jp>
2016-10-18 22:03:07 +0900

    ifftを使わずに復元するように

--- 6314089/03/play_a.m
+++ 6314089/03/play_a.m
@@ -2,14 +2,12 @@ function play_a (Afs, Fs, T)
     nAfs = length(Afs);
 
     N = 44100 * T;
-    y = zeros(1, N);
-    
+  
+    t = 0:(1/Fs):T;
+    y = zeros(1, length(t));
     for k = 1:nAfs
-        y(floor(k*((N/2)/nAfs))) = Afs(k);
-    end
-    for k = ceil(N/2):N
-        y(k) = conj(y(mod(N-k+1,N)+1));
+        Af = Afs(k);
+        y = y + abs(Af)*sin((2*pi*(27.5*k)*t) + angle(Af)) / 50000;
     end
     
-    x = ifft(y);
-    sound(x, Fs);
+    sound(y, Fs);
--- 6314089/03/test1.m
+++ 6314089/03/test1.m
@@ -2,12 +2,12 @@
 %path = '../Audio/A4_saw.wav';
 %path = '../Audio/A4_tri.wav';
 %path = '../Audio/A4_squ.wav';
-path = '../Audio/A4_tri_F4_tri.wav';
+%path = '../Audio/A4_tri_F4_tri.wav';
 
 [audio, Fs] = audioread(path);
 a4saw = A_frequencies(path);
 
 
 sound(audio, Fs);
-pause(2.5);
+pause(2.2);
 play_a(a4saw, Fs, 2);
