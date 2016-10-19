24bf89f8e79481a55ae39fad2bc1c45f0dcfd542
6314089 <6314089@ed.tus.ac.jp>
2016-10-19 09:11:17 +0900

    ピークの取得の仕方を変えてみる

--- 6314089/03/A_frequencies.m
+++ 6314089/03/A_frequencies.m
@@ -8,7 +8,7 @@ function ret = A_frequencies(filename)
     Y = fft(input, n);
     
     %get peek frequencies and length
-    [~, locs] = findpeaks(abs(Y), 'MinPeakProminence', 2500);
+    [~, locs] = findpeaks(abs(Y), 'MinPeakHeight', n/50);
     peekfs = (locs-1)*(Fs/n); %turn locs into "0 start" and into frequencies
     npeekfs = ceil(length(peekfs)/2);
     
@@ -19,18 +19,31 @@ function ret = A_frequencies(filename)
     %fill return value with 0
     ret = zeros(1, length(Afs));
 
-    %10cents
-    threshold = nthroot(2, 12*10);    
+    %1cents
+    threshold = nthroot(2, 12*100);    
 
     
     for i = 1:nAfs
         Af = Afs(i);
+        
+        pks = 0;
         for j = 1:npeekfs
             f= peekfs(j);
             if (Af >= f && Af / f <= threshold) ||...
-                (Af < f && f / Af <= threshold) %+-10cents
-               ret(i) = Y(locs(j)) / n; %
+                (Af < f && f / Af <= threshold) %+-1cents
+               pks = [pks Y(locs(j))];
             end
         end
+        
+        r = 0;
+        theta = 0;
+        for pk = pks
+            r = r + abs(pk / n);
+            theta = theta + angle(pk)/length(pks);
+        end
+        
+        ret(i) =r*cos(theta) + i * r*sin(theta);
     end
+    
+    max(abs(ret))
     
--- 6314089/03/test1.m
+++ 6314089/03/test1.m
@@ -1,8 +1,8 @@
-%path = '../Audio/A4_sin.wav';
+path = '../Audio/A4_sin.wav';
 %path = '../Audio/A4_saw.wav';
 %path = '../Audio/A4_tri.wav';
 %path = '../Audio/A4_squ.wav';
-path = '../Audio/A4_tri_F4_tri.wav';
+%path = '../Audio/A4_tri_F4_tri.wav';
 
 [audio, Fs] = audioread(path);
 a4saw = A_frequencies(path);
