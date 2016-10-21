57490f252ca1ee61d602806b41e391cedf14274e
Michael Laurents <you know@gmail.com>
2016-10-21 20:36:21 +0900

    動くようになった

--- Jsimluken/brute_force/brute_force.m
+++ Jsimluken/brute_force/brute_force.m
@@ -1,12 +1,18 @@
-function res = brute_force(orgSpectrum,srcs1,srcs2)
+function [i,j] = brute_force(orgSpectrum,srcs1,srcs2)
     size1 = size(srcs1);
+    size1 = size1(2);
     size2 = size(srcs2);
+    size2 = size2(2);
     res = zeros([size1,size2]);
     for i = 1:size1
         for j = 1:size2
-           [tmpSpectrum1,tmpSpectrum2] = atmseparater(orgSpectrum,srcs1(i),srcs2(j)) ;
+           [tmpSpectrum1,tmpSpectrum2] = atmSeparater(orgSpectrum,srcs1(:,i),srcs2(:,j)) ;
            res(i,j) =  (evalSpectrum(orgSpectrum,tmpSpectrum1)+...
-               evalSpectrum(orgSpectrum,tmpSpectrum2));
+               evalSpectrum(orgSpectrum,tmpSpectrum2))/2;
         end
     end
+    [M1,I1] = min(res);
+    [M2,I2] = min(M1);
+    i = I1(I2);
+    j = I2;
 return 
--- Jsimluken/brute_force/sample.m
+++ Jsimluken/brute_force/sample.m
@@ -1,21 +1,37 @@
-b = audioread('..Assets/B_A1.m4a');
-g = audioread('..Assets/G_E2.m4a');
-gb = audioread('..Assets/GB.m4a');
+b_a1 = audioread('../Assets/B_A1.m4a');
+b_b1 = audioread('../Assets/B_B1.m4a');
+b_g1 = audioread('../Assets/B_G1.m4a');
 
-b = b(20000:40000,1);
-g = g(20000:40000,1);
+g_a2 = audioread('../Assets/G_A2.m4a');
+g_d2 = audioread('../Assets/G_D2.m4a');
+g_e2 = audioread('../Assets/G_E2.m4a');
+g_f2 = audioread('../Assets/G_F2.m4a');
+
+gb = audioread('../Assets/GB.m4a');
+
+
+b_a1 = b_a1(20000:40000,1);
+b_b1 = b_b1(20000:40000,1);
+b_g1 = b_g1(20000:40000,1);
+
+g_a2 = g_a2(20000:40000,1);
+g_d2 = g_d2(20000:40000,1);
+g_e2 = g_e2(20000:40000,1);
+g_f2 = g_f2(20000:40000,1);
+
 gb = gb(20000:40000,1);
 
-sb = abs(fft(b));
-sg = abs(fft(g));
+sb_a1 = abs(fft(b_a1));
+sb_b1 = abs(fft(b_b1));
+sb_g1 = abs(fft(b_g1));
+sb = [sb_a1,sb_b1,sb_g1];
+
+sg_e2 = abs(fft(g_e2));
+sg_a2 = abs(fft(g_a2));
+sg_d2 = abs(fft(g_d2));
+sg_f2 = abs(fft(g_f2));
+sg = [sg_a2,sg_d2,sg_e2,sg_f2];
+
 sgb = abs(fft(gb));
 
-[res1,res2] = atmSeparater(sgb,sb,sg);
-eval1 = evalSpectrum(sb,res1);
-eval2 = evalSpectrum(sg,res2);
-
-%r1 = ifft(res1);
-%sound(r1,44100);
-%pause(1)
-%r2 = ifft(res2);
-%sound(r2,44100);
+[indexSb,indexSg] = brute_force(sgb,sb,sg);
