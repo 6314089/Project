aeaef5f7b420e6ba8a481e88d7e40cf8f3007542
6314089 <6314089@ed.tus.ac.jp>
2016-10-23 05:55:20 +0900

    正しく音量の増加部分の差分だけを取り出すように修正
    以前は逆に音量減少部分の差分を取り出していた…

--- 6314089/04/diff_p.m
+++ 6314089/04/diff_p.m
@@ -1,11 +1,6 @@
 function D = diff_p(v)
-    D = zeros(length(v) - 1);
-    
-    for k = 1:(length(D))
-        diff = v(k) - v(k + 1);
-        if diff >=0
-            D(k) = diff;
-        else
-            D(k) = 0;
-        end
-    end
+    vp = [0 v];
+    vp = vp(1:length(v));
+    diff = v - vp;
+    flag = diff >= 0;
+    D = diff .* flag;
