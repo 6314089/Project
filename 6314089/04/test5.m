f21312e37b6869a6ebee45e5db99e34227ced020
6314089 <6314089@ed.tus.ac.jp>
2016-10-23 07:38:40 +0900

    2つサンプルドラムパターンを足して実験してみる

---
+++ 6314089/04/test5.m
@@ -0,0 +1,8 @@
+[data, Fs] = audioread('../Audio/drums_120bpm.wav');
+bpm(data, Fs, 500)
+
+[data, Fs] = audioread('../Audio/drums_180bpm.wav');
+bpm(data, Fs, 500)
+
+[data, Fs] = audioread('../Audio/drums_165bpm.wav');
+bpm(data, Fs, 500)
