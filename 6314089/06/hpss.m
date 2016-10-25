cb49aaf0a977b433cdb525fb7c811d5f1b1b151b
6314089 <6314089@ed.tus.ac.jp>
2016-10-26 08:11:02 +0900

    冪指数も渡すようにした．また，スペクトログラムの復元も書いた

--- 6314089/06/hpss.m
+++ 6314089/06/hpss.m
@@ -1,6 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Y : 入力スペクトログラム
 %     （K*N行列，K: 周波数の分割数，N: 曲の長さ）
+% gamma : スペクトログラムの冪指数
 % w : P の滑らかさに対する重さ
 % u : Y と H+P の一致度に対する重さ
 % num : 更新回数
@@ -8,15 +9,17 @@
 % H : 調波成分のスペクトログラム（K*N行列）
 % P : 打楽器成分のスペクトログラム（K*N行列）
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [H, P] = hpss(Y, w, u, num)
+function [H, P] = hpss(Y, gamma, w, u, num)
 [K, N] = size(Y);
 Nyquist = ceil(K/2);
+YY = abs(Y).^gamma;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 初期値
-H = Y;
-P = Y;
+H = YY;
+P = YY;
 theta = zeros(Nyquist, N);
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 更新式をnum回繰り返す
@@ -33,7 +36,7 @@ for i = 1:num
             else
                 b = (H(k, n-1) + H(k, n+1));
             end
-            c = u * theta(k, n) * (Y(n, k))^2;
+            c = u * theta(k, n) * (YY(k, n))^2;
             H(k, n) = (b + sqrt(b^2 +a*c)) / a;
             
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -44,7 +47,7 @@ for i = 1:num
             else
                 b = (P(k-1, n) + P(k+1, n));
             end
-            c = u/w * (1 - theta(k, n)) * (Y(n, k))^2;
+            c = u/w * (1 - theta(k, n)) * (YY(k, n))^2;
             P(k, n) = (b + sqrt(b^2 +a*c)) / a;
             
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -53,6 +56,21 @@ for i = 1:num
         end
     end
 end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% スペクトログラムを復元する
+for n = 1:N
+    for k = 1:Nyquist
+        H(k, n) = H(k, n) .^ (1/gamma);
+        P(k, n) = P(k, n) .^ (1/gamma);
+        
+        r = angle(Y(k, n));
+        H(k, n) = complex(H(k, n) * cos(r), H(k, n) * sin(r));
+        H(k, n) = complex(P(k, n) * cos(r), P(k, n) * sin(r));
+    end
+end
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % ナイキスト数以降を共役対称にする
