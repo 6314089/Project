5a3666514eaff23e11bc3409f9be1735807a7f94
Michael Laurents <you know@gmail.com>
2016-10-28 16:44:06 +0900

    返す情報を増やした

--- Jsimluken/ICA/stereoICA/maxKurtosis.m
+++ Jsimluken/ICA/stereoICA/maxKurtosis.m
@@ -1,9 +1,16 @@
-function I = maxKurtosis(data)
+function [I,k1,k2] = maxKurtosis(data)
+    % data == k * n; k channels;
     tmp = zeros(360,1);
+    k1 = zeros(360,1);
+    k2 = zeros(360,1);
     for angle = 1:360
         U = TRolling(angle);
         Y = U * data;
-        tmp(angle,1) = sum(kurtosis(Y'));
+        t1 = kurtosis(Y(1,:));
+        t2 = kurtosis(Y(2,:));
+        tmp(angle,1) = t1 + t2;
+        k1(angle,1) = t1;
+        k2(angle,1) = t2;
     end
     [~,I] = max(tmp);
 return 
