41aa23cadf1f39266411a861fd7b68e4cb0f9073
Michael Laurents <you know@gmail.com>
2016-10-28 16:43:31 +0900

    誤差で収束させる作戦を考える

--- Jsimluken/ICA/fastICA/getUnitary.m
+++ Jsimluken/ICA/fastICA/getUnitary.m
@@ -9,15 +9,17 @@ function U = getUnitary(Z)
         for count = 1:10
             y(index,:) = U(index,:) * Z;
             tmp = y(index,:);
+            orgU = U(index,:);
             U(index,:) = expectedValue(G_(tmp),tmp)*U(index,:)...
                          - expectedValue(G(tmp)*(Z(index,:))',tmp);
             su = zeros(1,channel);
             for k = 1:index-1
                 su = su + (U(k,:)*U(index,:)')*U(k,:);
             end
-            disp(size(su));
-            
             U(index,:) = U(index,:) - su;
+            div = U(index,:)/ norm(U(index,:)) - orgU;
+            disp([index,count]);
+            disp(sum(div)/channel);
             U(index,:) = U(index,:)/ norm(U(index,:));
         end
     end
