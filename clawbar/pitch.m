0dd4fef43c8836146d560ce979d92136e51a1ccb
clawbar <6314022@ed.tus.ac.jp>
2016-11-10 00:33:29 +0900

    pitch関数作成中

--- clawbar/pitch.m
+++ clawbar/pitch.m
@@ -1,15 +1,15 @@
-
 function r = pitch(w)
 [a,b]=size(w);
 n=5;
-t=1000;
+t=300;
+p=zeros(1,b);
 for i=1:b
     l=[];
     [y,x] = findpeaks(w(1:(a/2),i));
     m=0;
     for j=1:length(x)
         if y(j) > t
-            l=[l,x(j)]
+            l=[l,x(j)];
             m=m+1;
         end
         if m == n
@@ -17,11 +17,40 @@ for i=1:b
         end
     end
     v=0;
-    m
     for k=1:m
-        v=v+l(k)/k;
+        v=v+l(k)*44100/8192/k;
     end
-    v=v/m
+    v=v/m*2
+    
+    P=[87.31 , 98 , 110 , 123.47 , 130.81 , 146.83 , 164.81];
+    
+    V=abs(v-P(3));
+    p(i)=1;%A=ra
+    if abs(v-P(4)) < V
+        p(i)=2;%B=si
+        V=abs(v-P(4));
+    end
+    if abs(v-P(5)) < V
+        p(i)=3;%C=do
+        V=abs(v-P(5));
+    end %
+    if abs(v-P(6)) < V
+        p(i)=4;%D=re
+        V=abs(v-P(6));
+    end    
+    if abs(v-P(7)) < V
+        p(i)=5;%E=mi
+        V=abs(v-P(7));
+    end
+    if abs(v-P(1)) < V
+        p(i)=6;%F=fa
+        V=abs(v-P(1));
+    end  
+    if abs(v-P(2)) < V
+        p(i)=7;%G=so
+        V=abs(v-P(2));
+    end        
+    
 end
-
+p
 end
--- clawbar/plotw.m
+++ clawbar/plotw.m
@@ -2,7 +2,7 @@ function plotw(w)
 [a,b]=size(w);
 for i=1:b
     subplot(b,1,i)
-    x=0:1:50;
+    x=0:1:100;
     y=w(x+1,i);
     plot(x,y)
 end
