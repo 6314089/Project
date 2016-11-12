9b27a9ad7ce79eb710986e9ce531019015aed5c9
clawbar <6314022@ed.tus.ac.jp>
2016-11-12 22:33:28 +0900

    中断

--- clawbar/pitch.m
+++ clawbar/pitch.m
@@ -20,7 +20,7 @@ for i=1:b
     for k=1:m
         v=v+l(k)*44100/4096/4/k;
     end
-    v=v/m*2
+    v=v/m*2;
     
     P=[87.31 , 92.5 , 98 , 103.83 , 110 , 116.54 , 123.47 , 130.81 , 138.59 , 146.83 , 155.56 , 164.81];
     %P(1)=F=fa,P(5)=A=ra
@@ -28,11 +28,49 @@ for i=1:b
     p(i)=0;
     for q=1:length(P)        
         if abs(v-P(q)) < V
-        p(i)=q;
-        V=abs(v-P(q));
+            
+             p(i)=q;
+             V=abs(v-P(q));
+        end
     end
+%{    
+    if p(i)==5        
+        A2=sample('A2.wav');
+        l2=[];
+        [y2,x2] = findpeaks(A2(1:(length(A2)/2)));
+        for j=1:length(x2)
+            if A2(j) > 1000
+                l2=[l2,x2(j)];
+            end
+            if length(l) == length(l2)
+                break
+            end
+        end
+        c=corrcoef(l,l2);
+        if abs(c(1,2)) > 0.8 
+            p(i)=205;
+        end
     end
-        
+    
+    if p(i)==7        
+        B2=sample('B2.wav');
+        l2=[];
+        [y2,x2] = findpeaks(B2(1:(length(B2)/2)));
+        for j=1:length(x2)
+            if B2(j) > 1000
+                l2=[l2,x2(j)];
+            end
+            if length(l) == length(l2)
+                break
+            end
+        end
+        c=corrcoef(l,l2)
+        if abs(c(1,2)) > 0.8 
+            p(i)=207;
+        end
+    end
+%}    
+    
 end
 p
 end
