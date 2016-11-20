7f2ca18dfbc89bf33eb044458f8ff6cfc19b3ccb
6314089 <6314089@ed.tus.ac.jp>
2016-11-20 15:51:11 +0000

    多重HPSSは打ち止め

---
+++ 6314089/08/test2.m
@@ -0,0 +1,25 @@
+%path = '../../ModelAudio/kaeru.wav';
+%path = '../../ModelAudio/kirakira.wav';
+path = '../../ModelAudio/Fuer Elise11.wav';
+
+frame1 = 4096;
+step1 = 2048;
+gamma1 = 0.3;
+w1 = 0.9;
+u1 = 1;
+n1 = 30;
+
+frame2 = 32768;
+step2 = 5000;
+gamma2 = 0.3;
+w2 = 1.1;
+u2 = 1;
+n2 = 30;
+
+audio = true;
+
+[~, ~, ~] = multi_hpss2(...
+    path, ...
+    frame1, step1, gamma1, w1, u1, n1, ...
+    frame2, step2, gamma2, w2, u2, n2, ...
+    audio);
