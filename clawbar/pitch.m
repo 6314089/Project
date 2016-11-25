21bdb64b0c06cf0be80f6b1089d21736cd5b3ef8
clawbar <6314022@ed.tus.ac.jp>
2016-11-25 14:03:10 +0900

    中途

--- clawbar/pitch.m
+++ clawbar/pitch.m
@@ -2,7 +2,7 @@ function r = pitch(w)
 [a,b]=size(w);
 n=5;
 t=300;
-p=zeros(1,b);
+r=zeros(1,b);
 for i=1:b
     l=[];
     [y,x] = findpeaks(w(1:(a/2),i));
@@ -25,52 +25,37 @@ for i=1:b
     P=[87.31 , 92.5 , 98 , 103.83 , 110 , 116.54 , 123.47 , 130.81 , 138.59 , 146.83 , 155.56 , 164.81];
     %P(1)=F=fa,P(5)=A=ra
     V=100;
-    p(i)=0;
+    p=0;
     for q=1:length(P)        
-        if abs(v-P(q)) < V
-            
-             p(i)=q;
+        if abs(v-P(q)) < V           
+             p=q;
              V=abs(v-P(q));
         end
     end
-%{    
-    if p(i)==5        
-        A2=sample('A2.wav');
-        l2=[];
-        [y2,x2] = findpeaks(A2(1:(length(A2)/2)));
-        for j=1:length(x2)
-            if A2(j) > 1000
-                l2=[l2,x2(j)];
+    C=0;
+    for s1=1:2
+        for s2=0:9
+            s=strcat('temp/',s1*1000+s2*100+p,'.wav')
+            if exist(s) ~= 0
+                S=sample(s);
+                l2=[];
+                [y2,x2] = findpeaks(S(1:(length(S)/2)));
+                for j=1:length(x2)
+                    if S(j) > 1000
+                        l2=[l2,x2(j)];
+                    end
+                    if length(l) == length(l2)
+                        break
+                    end
+                end
+                c=corrcoef(l,l2);
+                if abs(c(1,2)) > C
+                    C=abs(c(1,2))
+                    r(i)=s1*1000+s2*100+p;
+                end
             end
-            if length(l) == length(l2)
-                break
-            end
-        end
-        c=corrcoef(l,l2);
-        if abs(c(1,2)) > 0.8 
-            p(i)=205;
         end
     end
-    
-    if p(i)==7        
-        B2=sample('B2.wav');
-        l2=[];
-        [y2,x2] = findpeaks(B2(1:(length(B2)/2)));
-        for j=1:length(x2)
-            if B2(j) > 1000
-                l2=[l2,x2(j)];
-            end
-            if length(l) == length(l2)
-                break
-            end
-        end
-        c=corrcoef(l,l2)
-        if abs(c(1,2)) > 0.8 
-            p(i)=207;
-        end
-    end
-%}    
-    
 end
-p
+r
 end
--- clawbar/sample.m
+++ clawbar/sample.m
@@ -2,7 +2,11 @@ function r = sample(c)
 [y, fs] = audioread(c);
 % Convert to mono
 y = (y(:, 1) + y(:, 2)) / 2;
-y = fft(y);
+width = 4096*4;
+step = 2048*4;
+[stfted2, f2, t2] = stft(y, @hann, step, width, fs);
+r = abs(stfted2(:,12));
+phase2 = angle(stfted2);
 
-r = abs(y);
+%r = abs(y);
 end
