ac488b56352d70dbee05435cb486b6da9f4500d6
Jsimluken <simonsluke134@gmail.com>
2016-11-25 23:08:14 +0900

    drunk....

--- Jsimluken/NMF/nmf.m
+++ Jsimluken/NMF/nmf.m
@@ -1,7 +1,10 @@
 
 function [H, U] = nmf(Y, rank, h0, u0)
-H = rand(size(Y, 1), rank) + 0.1;
-U = rand(rank, size(Y, 2)) + 0.1;
+p = 0.5;
+gamma = 30;
+[O,T] = size(Y);
+H = rand(O, rank) + 0.1;
+U = rand(rank, T) + 0.1;
 if (exist('h0', 'var'))
     H(1:size(h0, 1), 1:size(h0, 2)) = h0;
 end
@@ -11,9 +14,24 @@ end
 avoidZero = 1e-27;
 
 for r = 1:100
+    for o = (1:O)
+        for d1 = (1:rank)
+            H(o,t) = h(o,d1) * (Y*U'/(H*(U*U')));
+        end
+    end
+    for d2 = (1:rank)
+       for t = 1:T
+           U(d2,t) = U(d2,t)*(H'*Y/(H'*H*U+gamma*p*abs(u(d2,t)^(p-2))));
+       end
+    end
+    
+    
+    
+    
+    
     % Eu dist ... seems legit
-    H = H .* (Y * U') ./ ((H * U) * U' + avoidZero);
-    U = U .* (H' * Y) ./ (H' * H * U + avoidZero);
+    %H = H .* (Y * U') ./ ((H * U) * U' + avoidZero);
+    %U = U .* (H' * Y) ./ (H' * H * U + avoidZero);
     
     % IS divergence ... not sure if this is correct
     %hui = (H * U) .^ -1;
