24ed82254885a3712905e41554f4fbbbe2357225
6314089 <6314089@ed.tus.ac.jp>
2016-11-28 00:02:20 +0900

    作業途中だけど眠いので寝る

--- 6314089/midi/test.js
+++ 6314089/midi/test.js
@@ -4,34 +4,15 @@ const MIDIFile = require('midifile');
 function toArrayBuffer(buffer) {
   const ab = new ArrayBuffer(buffer.length);
   const view = new Uint8Array(ab);
-  for (let i = 0, m = buffer.length; i < m; ++i) {
-    view[i] = buffer[i];
-  }
+  Array.from(buffer.entries()).forEach(([i, b]) => view[i] = b);
   return ab;
 }
 
-const mf = new MIDIFile(toArrayBuffer(fs.readFileSync('./test.mid')));
+// const mf = new MIDIFile(toArrayBuffer(fs.readFileSync('./test.mid')));
+const mf = new MIDIFile(toArrayBuffer(fs.readFileSync('./sakurasakura.mid')));
 
 const events = mf.getMidiEvents();
-console.log(events);
+events.length = 50;
+// console.log(events);
 
-
-const NOTE_OFF = 8;
-const NOTE_ON = 9;
-
-const obj = {};
-
-for (let event of events) {
-  if (event.type === 8) {
-    obj[event.track] = obj[event.track] || {};
-    obj[event.track][event.param1] = obj[event.track][event.param1] || [];
-
-    const arr = obj[event.track][event.param1];
-    const length = arr.length;
-
-    const index = Math.floor(event.playTime * 44100 / 1000);
-    if (event.subtype === NOTE_OFF) {
-
-    }
-  }
-}
+events.map(e => `time: ${e.playTime} [track: ${e.track}] subtype: ${e.subtype}, param1: ${e.param1}, param2: ${e.param2}`).forEach(console.log.bind(console));
