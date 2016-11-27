7e1615c2cef4cfeb175c439547835b13c106c7b3
6314089 <6314089@ed.tus.ac.jp>
2016-11-27 20:46:27 +0900

    書きかけ

--- 6314089/midi/test.js
+++ 6314089/midi/test.js
@@ -14,3 +14,24 @@ const mf = new MIDIFile(toArrayBuffer(fs.readFileSync('./test.mid')));
 
 const events = mf.getMidiEvents();
 console.log(events);
+
+
+const NOTE_OFF = 8;
+const NOTE_ON = 9;
+
+const obj = {};
+
+for (let event of events) {
+  if (event.type === 8) {
+    obj[event.track] = obj[event.track] || {};
+    obj[event.track][event.param1] = obj[event.track][event.param1] || [];
+
+    const arr = obj[event.track][event.param1];
+    const length = arr.length;
+
+    const index = Math.floor(event.playTime * 44100 / 1000);
+    if (event.subtype === NOTE_OFF) {
+
+    }
+  }
+}
