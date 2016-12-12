f376620013984cd0224586e18881094375866cdc
kurezoul <6314031@ed.tus.ac.jp>
2016-12-12 20:57:44 +0900

    Revert "© hori"
    This reverts commit 4e8221434b6915e7bb509836342b51986ce7cf6f.

--- kurezoul/bonding/HPSS/main.m
+++
@@ -1,2 +0,0 @@
-function main(path)
-hpss(path, 4096, 2048, 0.6, 1.1, 1, 30);
--- kurezoul/bonding/HPSS/mono.m
+++
@@ -1,6 +0,0 @@
-function mono = mono(data)
-if size(data, 2) == 2
-    mono = (data(:, 1) + data(:, 2)) / 2;
-else
-    mono = data;
-end
