b2d7f0e7b1a18594228a34f48136e1368791fd5e
Jsimluken <simonsluke134@gmail.com>
2016-12-14 15:51:33 +0900

    使い勝手をよくした

--- Jsimluken/midi/midiExpoter.py
+++ Jsimluken/midi/midiExpoter.py
@@ -2,7 +2,7 @@
 import midi
 import pretty_midi
 import sys
-
+import os
 
 def get_chroma(c):
     m = c % 12
@@ -45,7 +45,7 @@ def write_p_roll(p_roll,i,prefix):
     print prefix
     chroma = get_chroma(i)
     octave = get_octave(i)
-    f_name = prefix+chroma+str(octave)+".csv"
+    f_name = prefix+"/"+chroma+str(octave)+".csv"
     f = open(f_name,"w")
     f.write(data)
     f.close()
@@ -53,18 +53,22 @@ def write_p_roll(p_roll,i,prefix):
 if __name__ == "__main__":
     args = sys.argv
     if len(args) <2:
-        print "need filename"
+        print "need directory!!"
         exit()
-    filename = args[1]
-    prefix = filename.split('.')[0]
-    fs = 44100
-    if len(args) >2:
-        fs = int(args[2])
-    midi_data = pretty_midi.PrettyMIDI(filename)
-    p_roll = midi_data.get_piano_roll(fs)
+    dir_name = args[1]
+    os.chdir(dir_name)
+    instruments = ["bass","drum","piano"]
+    for instrument in instruments:
+        filename = instrument + ".mid"
+        prefix = instrument 
+        fs = 44100
+        if len(args) >2:
+            fs = int(args[2])
+        midi_data = pretty_midi.PrettyMIDI(filename)
+        p_roll = midi_data.get_piano_roll(fs)
     
-    active_Chroma = []
-    for i in range(128):
-        if max(p_roll[i]) > 0:
-            write_p_roll(p_roll[i],i,prefix)
+        active_Chroma = []
+        for i in range(128):
+            if max(p_roll[i]) > 0:
+                write_p_roll(p_roll[i],i,prefix)
 
