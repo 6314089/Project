6eaf0c3eb39c1812c1fe3a6ec6b03846d05951c8
Jsimluken <simonsluke134@gmail.com>
2016-12-15 19:20:03 +0900

    copywrite © 2016 tsurugaya 多分途中のやつ 上げないとエラー

--- Jsimluken/NMF/makeLabeledData.m
+++ Jsimluken/NMF/makeLabeledData.m
@@ -1,23 +1,36 @@
-function [F,sg] = makeLabeledData()
+function [F, ins] = makeLabeledData(sourceDir, instruments)
+    ext = '.wav';
     omega = 4096;
     command = '';
     if ismac
-        command = 'ls F/';
+        command = 'ls *.wav';
     else
-        command = 'dir /b F';
+        command = 'dir /b ';
     end
-    [~,cmdout] = unix(command);
-    files = strsplit(cmdout);
-    F = zeros(omega,size(files,2)-1);
-    sg = zeros(omega,size(files,2)-1);
-    for i = 1:(size(files,2)-1)
-        name = strcat('F/',char(files(i)));
-        ad = audioread(name);
-        if size(ad,2) >1
-            ad = (ad(:,1) + ad(:,2))/2;
+    F = [];
+    ins = cell(length(instruments));
+    for j = 1:length(instruments)
+        subDir = instruments(j);
+        targetDir = strcat(sourceDir, '/', subDir);
+        [~,cmdout] = unix(strcat(command, subDir));
+        files = strsplit(cmdout);
+        % Discard files w/o a specific extension
+        files = files(endsWith(files, ext));
+        preSize = size(F, 2);
+        additionalSize = size(files, 2);
+        F = [F, zeros(omega, additionalSize)];
+        tempIns = zeros(1, additionalSize);
+        for i = 1:(size(files,2)-1)
+            fileName = char(files(i));
+            tempIns(i) = strcat(subDir, fileName(1:length(fileName) - length(ext)));
+            name = strcat(targetDir, fileName);
+            ad = audioread(name);
+            if size(ad,2) >1
+                ad = (ad(:,1) + ad(:,2))/2;
+            end
+            st = fft(ad(1:omega)); 
+            F(:, i + preSize) = abs(st)';
         end
-        st = fft(ad(1:omega)); 
-        F(:,i) = abs(st)';
-        sg(:,i) = st';
+        ins(j) = tempIns;
     end
 return
