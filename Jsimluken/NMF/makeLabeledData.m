0618cd63b718d3d9e82e8ea3bb15bfc2366ebf76
Jsimluken <simonsluke134@gmail.com>
2016-12-16 14:13:37 +0900

    copywrite © 2016 tsurugaya

--- Jsimluken/NMF/makeLabeledData.m
+++ Jsimluken/NMF/makeLabeledData.m
@@ -2,35 +2,47 @@ function [F, ins] = makeLabeledData(sourceDir, instruments)
     ext = '.wav';
     omega = 4096;
     command = '';
+    slash = '';
     if ismac
-        command = 'ls *.wav';
+        command = 'ls';
+        slash = '/';
     else
-        command = 'dir /b ';
+        command = 'dir /b';
+        slash = '\';
     end
     F = [];
-    ins = cell(length(instruments));
-    for j = 1:length(instruments)
-        subDir = instruments(j);
-        targetDir = strcat(sourceDir, '/', subDir);
-        [~,cmdout] = unix(strcat(command, subDir));
+    ins = cell(size(instruments, 1), 1);
+    % Loop for instruments
+    for j = 1:size(instruments, 1)
+        subDir = instruments{j};
+        targetDir = strcat(sourceDir, slash, subDir, slash);
+        [~,cmdout] = unix(strcat(command, [' ', targetDir, '*', ext]));
         files = strsplit(cmdout);
-        % Discard files w/o a specific extension
-        files = files(endsWith(files, ext));
-        preSize = size(F, 2);
-        additionalSize = size(files, 2);
+        currentSize = size(F, 2);
+        additionalSize = size(files, 1);
         F = [F, zeros(omega, additionalSize)];
-        tempIns = zeros(1, additionalSize);
+        tempIns = lower(subDir(1:2));   % The name of the instument of this loop
+        disp(tempIns);
+        % Loop for pitch
         for i = 1:(size(files,2)-1)
-            fileName = char(files(i));
-            tempIns(i) = strcat(subDir, fileName(1:length(fileName) - length(ext)));
-            name = strcat(targetDir, fileName);
+            name = char(files(i));  % ???
+            if ismac
+                slsPos = strfind(name, '/');    % Temp var to get file name
+                fileName = name(slsPos(length(slsPos)) + 1:length(name) - length(ext));
+            else
+                fileName = name(1:length(name) - length(ext));
+                name = [targetDir, name];
+            end
+            %disp(tempIns);
+            %disp(fileName);
+            tempIns = [tempIns; fileName];  % Don't know how to create a char array with specific length
             ad = audioread(name);
             if size(ad,2) >1
                 ad = (ad(:,1) + ad(:,2))/2;
             end
             st = fft(ad(1:omega)); 
-            F(:, i + preSize) = abs(st)';
+            F(:, i + currentSize) = abs(st)';
         end
-        ins(j) = tempIns;
+        ins{j} = tempIns;
     end
 return
